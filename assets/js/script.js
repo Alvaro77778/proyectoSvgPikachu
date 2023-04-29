151d9d57cd692fabd8fe7abf40d0e17be3852353
Alvaro77778 <bounce15_vito@hotmail.com>
2023-04-29 02:03:14 -0400

    picachu

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function cambiarColorPikachu(){
+    let cambiarColorLengua = document.getElementById("path2720")
+    cambiarColorLengua.style.fill = "#191970"
+
+    let cambiarColorOjos = document.getElementById("path2498")
+    cambiarColorOjos.style.fill = "#ADFF2F"
+
+    let cambiarColorMejillas = document.getElementById("path2510")
+    cambiarColorMejillas.style.fill = "#191970"
+
+    let cambiarColorCuerpo = document.getElementById("path2462")
+    cambiarColorCuerpo.style.fill = "#800080" 
+
+    let cambiarColorContorno = document.getElementById("path2423")
+    cambiarColorContorno.style.fill = "#00FA9A"
+
+
+}
+
+function volverAPikachu(){
+    let cambiarColorLengua = document.getElementById("path2720")
+    cambiarColorLengua.style.fill = "#ee7277"
+
+    let cambiarColorOjos = document.getElementById("path2498")
+    cambiarColorOjos.style.fill = "#ffffff"
+
+    let cambiarColorMejillas = document.getElementById("path2510")
+    cambiarColorMejillas.style.fill = "#ee4e36"
+
+    let cambiarColorCuerpo = document.getElementById("path2462")
+    cambiarColorCuerpo.style.fill = "#ffd73e" 
+
+    let cambiarColorContorno = document.getElementById("path2423")
+    cambiarColorContorno.style.fill = "#000000"
+}
